f9dea34c96b84b74b2c22736587928f01ba221b6
FxReturned <cesar.cor.itu@gmail.com>
2025-01-16 08:01:57 -0500

    ex3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Console.Write("What is the magic number? ");
+        int magicNumber = int.Parse(Console.ReadLine());
+
+        int guessnumber = -1;
+        int tries = 1;
+
+        while (guessnumber != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            guessnumber = int.Parse(Console.ReadLine());
+
+            if (magicNumber > guessnumber)
+            {
+                Console.WriteLine("Higher");
+                tries += 1;
+            }
+            else if (magicNumber < guessnumber)
+            {
+                Console.WriteLine("Lower");
+                tries += 1;
+            }
+            else
+            {
+                Console.WriteLine("You got it!");
+                Console.WriteLine($"Your number of attempts was {tries}!");
+            }
+
+
+        }                 
     }
 }
