9b6045fa9781d69794e716330b946a598bcf5061
FxReturned <cesar.cor.itu@gmail.com>
2025-01-16 07:51:01 -0500

    update

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,9 +1,60 @@
 using System;
+using System.Reflection.Metadata;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+Console.Write("What is your grade percentage? ");
+        string answer = Console.ReadLine();
+        int percent = int.Parse(answer);
+        int lastdigit = percent % 10;
+
+        string letter = "";
+        string sign = "";
+
+        if (percent >= 90)
+        {
+            letter = "A";
+        }
+        else if (percent >= 80)
+        {
+            letter = "B";
+        }
+        else if (percent >= 70)
+        {
+            letter = "C";
+        }
+        else if (percent >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+     
+        if (lastdigit >= 7 && letter != "A" && letter != "F")
+        {
+             sign = "+";
+        }
+        else if (lastdigit < 3 && letter != "F")
+        {
+             sign = "-";
+        }
+        
+
+
+        Console.WriteLine($"Your grade is: {letter}{sign}");
+        
+        if (percent >= 70)
+        {
+            Console.WriteLine("You passed! Good Job!");
+        }
+        else
+        {
+            Console.WriteLine("try harder next time!");
+        }
     }
 }
