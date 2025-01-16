840f55e0f52c868a3b563f84bb8883860270266f
FxReturned <cesar.cor.itu@gmail.com>
2025-01-16 11:48:03 -0500

    exc5

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -1,9 +1,48 @@
 using System;
 
 class Program
-{
+{    
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        WelcomeMessage();
+
+        string Name = PromptName();
+        int Number = PromptNumber();
+
+        int squaredNumber = SquareNumber(Number);
+
+        DisplayResult(Name, squaredNumber);
+    }
+
+    static void WelcomeMessage()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    static string PromptName()
+    {
+        Console.Write("Please enter your name: ");
+        string promptName = Console.ReadLine();
+
+        return promptName;
+    }
+
+    static int PromptNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        int promptNumber = int.Parse(Console.ReadLine());
+
+        return promptNumber;
+    }
+
+    static int SquareNumber(int promptNumber)
+    {
+        int square = promptNumber * promptNumber;
+        return square;
+    }
+
+    static void DisplayResult(string promptName, int square)
+    {
+        Console.WriteLine($"{promptName}, the square of your number is {square}");
     }
 }
